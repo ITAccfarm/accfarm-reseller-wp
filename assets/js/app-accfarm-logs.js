bbdb595678f3431233d0ead192d8fb0c8d2ac994
Vladislav Kushch <kushch.vladislav@gmail.com>
2022-04-19 16:54:35 +0300

    js log fixes

--- assets/js/app-accfarm-logs.js
+++ assets/js/app-accfarm-logs.js
@@ -1,7 +1,3 @@
 jQuery(document).ready(function ($) {
-    $(window).scroll(function () {
-        if ($(document).height() - $(this).height() === $(this).scrollTop()) {
-            alert('Scrolled to Bottom');
-        }
-    });
+
 });
